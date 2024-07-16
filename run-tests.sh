e8f1b56078a6c5e1af4e69559e7677de838c8472
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-07-16 21:36:59 +0200

    global: update workflows/tests.yml

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,7 @@
 # Invenio-Cli is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-npx markdownlint-cli docs/* && \
-awesome_bot --allow-dupe --skip-save-results --allow-redirect docs/**/*.md && \
+npx markdownlint-cli docs/*
+awesome_bot --allow-dupe --skip-save-results --allow-redirect docs/**/*.md
 mkdocs build -v
 rm -rf site/
