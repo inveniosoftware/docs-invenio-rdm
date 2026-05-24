67556ebffbb65b46918b87a13db13edc42218e55
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-24 22:05:09 +0200

    chore(licenses): update license headers to use SPDX

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2019 CERN.
-# Copyright (C) 2019 Northwestern University.
-#
-# Invenio-Cli is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2019 CERN.
+# SPDX-FileCopyrightText: 2019 Northwestern University.
+# SPDX-License-Identifier: MIT
 
 npx markdownlint-cli docs/*
 awesome_bot --allow-dupe --skip-save-results --allow-redirect docs/**/*.md
